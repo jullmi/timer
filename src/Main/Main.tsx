d9fc515be882405fd9408dc486b8196a2b73bdb3
Julia Savchenko <iusavchenko@dinord.ru>
2024-01-21 16:03:47 +0400

    added styles and removed unused libraries

--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -2,15 +2,18 @@ import React, { useState } from 'react';
 
 import { addTimer, removeTimer } from './behavior'
 import Timer from "../Timer";
+import './styles.css'
 
 function Main() {
 	const [timers, setTimers] = useState<number[]>([])
 
 	return (
-		<div>
-			<button onClick={() => addTimer(setTimers)}>Add timer</button>
-			<button onClick={() => removeTimer(setTimers)}>Remove</button>
-			<ul style={{display: "flex", gap: 16}}>
+		<div className="root">
+			<div className="buttons_wrapper">
+				<button onClick={() => addTimer(setTimers)}>Add timer</button>
+				<button onClick={() => removeTimer(setTimers)}>Remove</button>
+			</div>
+			<ul className="list">
 				{timers.map(timer => (
 					<Timer key={timer}/>
 				))}
--- src/Timer/Timer.tsx
+++ src/Timer/Timer.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Duration } from 'luxon'
+import { Duration } from "luxon";
 
 import { enableTimer, resetTimer, useTimerValue } from './behavior'
+import './styles.css'
 
 function Timer() {
 	const emptyDuration = Duration.fromObject({minutes: 0, seconds: 0, milliseconds: 0});
@@ -11,10 +12,12 @@ function Timer() {
 	const value = useTimerValue(isTimerEnable, timerValue, setTimerValue)
 
 	return (
-		<div style={{border: "1px solid black", padding: 16, width: "max-content"}}>
-			<div>{`${value.minutes}:${value.seconds}.${value.milliseconds}`}</div>
-			<button onClick={() => enableTimer(setIsTimerEnable)}>Start / Pause</button>
-			<button onClick={() => resetTimer(setTimerValue, emptyDuration)}>Reset</button>
+		<div className="timer">
+			<div className="timer_value">{`${value.minutes}:${value.seconds}.${value.milliseconds}`}</div>
+			<div className='timer_buttons_container'>
+				<button onClick={() => enableTimer(setIsTimerEnable)}>Start / Pause</button>
+				<button onClick={() => resetTimer(setTimerValue, emptyDuration)}>Reset</button>
+			</div>
 		</div>
 	);
 }
